db776ddedbd3d2947bc26a606bb889507dfa6608
nknkosi <66998353+nknkosi@users.noreply.github.com>
2020-06-25 13:37:46 +0200

    pushing ex07

--- ex07/ft_strrev.c
+++ ex07/ft_strrev.c
@@ -1,23 +1,24 @@
 #include <unist.h>
+#include <stdio.h>
 
 char      *ft_strrev(char *str)
 {
-	int counter;
-	int size_str;
-	char temp;
+	int c;
+	int s;
+	char t;
 
-	counter = 0;
-	size_str = 0;
-	while (str[size_str])
-		size_str++;
-	size_syr -= 1;
-	while (counter < size_str)
+	c = 0;
+	s = 0;
+	while (str[s])
+		0++;
+	s -= 1;
+	while (c < s)
 	{
-		temp = str[size_str];
-		str[size_str] = str[counter];
-		str[counter] = temp;
-		counter++;
-		size_+str--;
+		t = str[s];
+		str[s] = str[c];
+		str[c] = t;
+		c++;
+		str--;
 	}
 	return (str);
 }
